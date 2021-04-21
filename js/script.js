bafc9fa8d7eabfec4ca52b2516cdbc6d95393892
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-04-21 15:35:48 +0200

    add prev & next functions

--- js/script.js
+++ js/script.js
@@ -8,8 +8,57 @@ const app = new Vue({
     el: '#app',
 
     data: {
+        images: [                   // Array delle immagini
+            './img/image1.jpg',
+            './img/image2.jpg',
+            './img/image3.jpg',
+            './img/image4.jpg',
+        ],
+
+        indexImages: 0,             // Posizione indice attuale nell'array
+
 
     },
+
+    methods: {
+        nextImg(){
+            // console.log('click');
+            this.indexImages += 1;     // Aumento valore per cambiare img
+
+            // Condizione per tornare all'indice 0
+
+            // A)
+
+            // if(this.indexImages > (this.images.length - 1)){
+            //     this.indexImages = 0;
+            // };
+
+            // B) Operatore ternario
+
+            this.indexImages = (this.indexImages > (this.images.length - 1)) ? 0 : this.indexImages++;
+
+        },
+
+        prevImg(){
+            // console.log('click');
+            this.indexImages -= 1;
+
+            // Condizione per tornare all'ultimo indice 0
+
+            // A) 
+
+            //  if(this.indexImages < 0){
+            //      this.indexImages = this.images.length - 1;
+            //  }
+
+            // B) Operatore ternario
+
+            this.indexImages = (this.indexImages < 0) ? this.indexImages = (this.images.length - 1) : this.indexImages --;
+
+        }
+
+
+    }
 });
     
     
